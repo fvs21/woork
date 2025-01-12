66411064e1402f3bed0bc08cc944b2389b91f572
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-01-12 12:46:48 -0600

    Implemented ui design in the id verification form

--- frontend/src/app/Features/verification/components/VerificationSteps/VerificationTwo.tsx
+++ frontend/src/app/Features/verification/components/VerificationSteps/VerificationTwo.tsx
@@ -11,11 +11,24 @@ export default function VerificationTwo({setStep}) {
                     <div className={styles.idsFormDescription}>
                         Deberás subir una foto de la parte delantera y trasera de tu identificación oficial con el CURP visible, teniendo en cuenta los siguientes aspectos:
                         <ul className={styles.requirementsList}>
-                            <li>Debe ser <b>válida</b> y estar <b>vigente</b>.</li>
-                            <li>El documento sea el original.</li>
-                            <li>La foto sea lo más clara posible, asegurandose que muestre toda la información.</li>
-                            <li></li>
+                            <li className={styles.requirement}>Debe ser <b>válida</b> y estar <b>vigente</b>.</li>
+                            <li className={styles.requirement}>El documento sea el original.</li>
+                            <li className={styles.requirement}>La foto sea lo más clara posible, asegurandose que muestre toda la información.</li>
                         </ul>
+                    </div>
+                    <div className={styles.idsSubmitFormsContainer}>
+                        <div className={styles.idSubmitForm}>
+                            <label className={styles.formLabel}>Parte delantera</label>
+                            <div className={styles.idSubmit}>
+
+                            </div>
+                        </div>
+                        <div className={styles.idSubmitForm}>
+                            <label className={styles.formLabel}>Parte trasera</label>
+                            <div className={styles.idSubmit}>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
