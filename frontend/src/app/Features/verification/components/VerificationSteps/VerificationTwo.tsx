33a312865d49fe2e9780dfda0b1368e74f195479
Fabrizio Vanzani <vanzanifabrizio@gmail.com>
2025-01-12 11:54:33 -0600

    Started implementing id submission form

--- frontend/src/app/Features/verification/components/VerificationSteps/VerificationTwo.tsx
+++ frontend/src/app/Features/verification/components/VerificationSteps/VerificationTwo.tsx
@@ -1,7 +1,24 @@
+import styles from "./Verification.module.scss";
+
 export default function VerificationTwo({setStep}) {
     return (
-        <div>
-            
-        </div>
+        <section className={styles.verificationContainer}>
+            <div className={styles.verificationFormsContainer}>
+                <div className={styles.idsForm}>
+                    <div className={styles.idsFormTitle}>
+                        Identificación oficial INE o IFE
+                    </div>
+                    <div className={styles.idsFormDescription}>
+                        Deberás subir una foto de la parte delantera y trasera de tu identificación oficial con el CURP visible, teniendo en cuenta los siguientes aspectos:
+                        <ul className={styles.requirementsList}>
+                            <li>Debe ser <b>válida</b> y estar <b>vigente</b>.</li>
+                            <li>El documento sea el original.</li>
+                            <li>La foto sea lo más clara posible, asegurandose que muestre toda la información.</li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
